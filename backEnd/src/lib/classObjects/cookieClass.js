8c095bce5805783f4df1cdefb97103253c8b7a93
kinshuksri25 <42490429+kinshuksri25@users.noreply.github.com>
2020-03-02 11:55:33 +0530

    code refactor (backend) -- adding cookieClass.js

---
+++ backEnd/src/lib/classObjects/cookieClass.js
@@ -0,0 +1,20 @@
+//Blueprint for cookie Objects
+
+class cookies = {
+  constructor(cookieName = "",creationTime = Date.now()){
+    this.cookieName = cookieName;
+    this.creationTime = creationTime;
+  }
+  
+  getCookie(){
+    return ({cookieName:this.cookieName, creationTime:this.creationTime});
+  }
+  
+  setCookie(cookieName = this.cookieName,creationTime = this.creationTime){
+    this.cookieName = cookieName;
+    this.creationTime = creationTime;
+  }
+};
+
+//export the module
+module.exports = cookies;
