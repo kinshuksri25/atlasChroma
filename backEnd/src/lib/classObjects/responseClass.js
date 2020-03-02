cb50c2a21672466be3680c6f875d756bd478af5d
kinshuksri25 <42490429+kinshuksri25@users.noreply.github.com>
2020-03-02 12:12:11 +0530

    code refactor (backend) --  responseClass.js

---
+++ backEnd/src/lib/classObjects/responseClass.js
@@ -0,0 +1,26 @@
+//Blueprint for response Object 
+
+class responseObject(){
+
+  constructor(status = 500,smsg = "", payload = {}, emsg = ""){
+    this.status = status;
+    this.smsg =  smsg;
+    this.payload = payload;
+    this.emsg = emsg;
+  }
+  
+  getResponseObject(){
+    return({STATUS: this.status,SUCCESSMSG: this.smsg,PAYLOAD: this.payload,ERRORMSG: this.emsg});
+  }
+  
+  setResponseObject(status = this.status,smsg = this.smsg,payload = this.payload,emsg = this.emsg){
+    this.status = status;
+    this.smsg =  smsg;
+    this.payload = payload;
+    this.emsg = emsg;
+  }
+
+};
+
+//export the class
+module.exports = responseObject;
