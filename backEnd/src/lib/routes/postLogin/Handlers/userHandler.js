c389929fc010f7693bfe4ae9cccf4c8b7496aae0
kinshuksri25 <42490429+kinshuksri25@users.noreply.github.com>
2020-03-03 19:32:55 +0530

    code refactor (backend) -- userHandler.js

--- backEnd/src/lib/routes/postLogin/Handlers/userHandler.js
+++ backEnd/src/lib/routes/postLogin/Handlers/userHandler.js
@@ -2,7 +2,31 @@
 
 
 //declaring the module
-const userHandler = require("");
+const userHandler = {};
+
+
+userHandler.user = (requestObject) => new Promise((resolve,reject) => {
+    let response = {};
+    if(requestObject.hasOwnProperty("method")){
+        swtich(requestObject.method){
+          case "GET" :
+              userHandler.user.get.then(resolvedResult => {
+              }).catch(rejectedResult => {
+              });
+              break;
+          case "POST" : 
+              break;
+          case "PUT" : 
+              break;
+          case "DELETE" : 
+              break;
+        }
+    }else{
+      response.STATUS = 500;
+      response.EMSG = "METHOD NOT FOUND!";
+      reject(response);
+    }
+});
 
 
 
