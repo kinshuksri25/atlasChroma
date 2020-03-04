9b0b45d08c5bc8516594a6bb6e2f3df98dd85578
kinshuksri25 <42490429+kinshuksri25@users.noreply.github.com>
2020-03-04 16:25:40 +0530

    code refactor (backend) -- updated centralRouter.js

--- backEnd/src/lib/routes/centalRouter.js
+++ backEnd/src/lib/routes/centalRouter.js
@@ -1,10 +1,8 @@
-
 //Dependencies
-const preLoginRouter = require("");
-const postLoginRouter = require("");
-const cookieHandler = require("");
+const preLoginRouter = require("./preLogin/preLoginRouter");
+const postLoginRouter = require("postLogin/postLoginRouter");
 
-//defining the router object
+//defining the module
 const router = {};
 
 //params --> route -- string, requestObject -- object
@@ -24,36 +22,13 @@ router.router = (route,requestObject) => new Promise((resolve,reject) => {
 
     }else{
         //postlogin
-        //check cookie availability
-        let response = {};
-        if(requestObject.hasOwnProperty("cookieObject")){
-            cookieHandler.checkCookie(requestObject.cookieObject).then(validCookie => {
-                if(validCookie){
-                    postLoginRouter(route,requestObject).then(resolvedResult => {
-                        resolve(resolvedResult);
-                    }).catch(rejectedResult => {
-                        reject(rejectedResult);
-                    });
-                }else{
-                    response.STATUS = 400;
-                    response.EMSG = "INVALID COOKIE DATA FOUND";
-                    //send to login page
-                    reject(response);
-                }
-            }).catch(rejectedResult => {
-                response.STATUS = 500;
-                response.EMSG = rejectedResult;
-                //send to login page
-                reject(response);
-            });
-        }else{
-            response.STATUS = 400;
-            response.EMSG = "COOKIE NOT FOUND";
-            //send to login page
-            reject(response);
-        }
-    }
+          postLoginRouter.router(route,requestObject).then(resolvedResult => {
+                resolve(resolvedResult);
+          }).catch(rejectedResult => { 
+                reject(rejectedResult);
+          });
+       }
 
-});
-
+}); 
+ 
 module.exports = router;
