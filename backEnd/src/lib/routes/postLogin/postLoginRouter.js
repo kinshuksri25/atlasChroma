65a78efb638f5968bb11083486badaab57f31075
kinshuksri25 <42490429+kinshuksri25@users.noreply.github.com>
2020-03-04 16:34:46 +0530

    code refactor (backend) -- postLoginRouter.js

--- backEnd/src/lib/routes/postLogin/postLoginRouter.js
+++ backEnd/src/lib/routes/postLogin/postLoginRouter.js
@@ -1,31 +1,45 @@
 //Dependencies
-const userHandler = require("");
-const projectHandler = require("");
-const responseObject = require("");
-const {MSG} = require("");
+const userHandler = require("./Handlers/userHandler");
+const projectHandler = require("./Handlers/projectsHandler");
+const responseObject = require("../../classObjects/responseClass");
 
-//defining the postLoginRouter object
+//defining the module
 const postLoginRouter = {};
 
 //params --> route -- string, requestObject -- object
 //returns --> promise(object)
 postLoginRouter.router = (route,requestObject) = new Promise((resolve,reject) => {
-
-    let chosenHandler = postLoginRouter.routes.hasOwnProperty(route) ? postLoginRouter.routes.handlers[route] : postLoginRouter.routes.handlers.notFound;
-    chosenHandler(requestObject).then(resolvedResult => {
-        let response = new responseObject(resolvedResult.STATUS,resolvedResult.SMSG,resolvedResult.PAYLOAD,MSG.EMSG.NOERROR);
-        resolve(response.getResponseObject());
-    }).catch(rejectedResult => {
-        let response = new responseObject(rejectedResult.STATUS,MSG.SMSG.NOSUCCESS,{},rejectedResult.EMSG);
-        reject(response.getResponseObject());
-    });
-
+      //check cookie availability
+        if(requestObject.hasOwnProperty("cookieObject")){
+            cookieHandler.checkCookie(requestObject.cookieObject).then(validCookie => {
+                if(validCookie){
+                    let chosenHandler = postLoginRouter.routes.hasOwnProperty(route) ? postLoginRouter.routes.handlers[route] : postLoginRouter.routes.handlers.notFound;
+                    chosenHandler(requestObject).then(resolvedResult => {
+                        let response = new responseObject(resolvedResult.STATUS,resolvedResult.SMSG,resolvedResult.PAYLOAD,MSG.EMSG.NOERROR);
+                        resolve(response.getResponseObject());
+                    }).catch(rejectedResult => {
+                        let response = new responseObject(rejectedResult.STATUS,MSG.SMSG.NOSUCCESS,{},rejectedResult.EMSG);
+                        reject(response.getResponseObject());
+                    });
+                }else{
+                    let response = new responseObject(400,MSG.SMSG.NOSUCCESS,{},"INVALID COOKIE DATA FOUND");
+                    reject(response);
+                }
+            }).catch(rejectedResult => {
+                let response = new responseObject(500,MSG.SMSG.NOSUCCESS,{},rejectedResult);
+                reject(response);
+            });
+        }else{
+            let response = new responseObject(400,MSG.SMSG.NOSUCCESS,{},"COOKIE DATA NOT FOUND");
+            reject(response);
+        }
 });
 
-//prelogin routes
+//postlogin routes
 postLoginRouter.routes = {
  "/user" : userHandler.user,
  "/project" : projectHandler.project
 };
 
+//export the module
 module.exports = postLoginRouter;
