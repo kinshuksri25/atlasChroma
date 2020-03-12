d53eac3e51cf3f5c5de554a1cbc300a13dca51fc
kinshuksri25 <42490429+kinshuksri25@users.noreply.github.com>
2020-03-12 11:34:54 +0530

    code refactor (backend) -- userClass.js

--- backEnd/src/lib/classObjects/userClass.js
+++ backEnd/src/lib/classObjects/userClass.js
@@ -4,42 +4,42 @@ const {randValueGenerator} = require("../utils/helper");
 //Blueprint for the user objects
 class userObject{
   constructor({_id = randValueGenerator(), 
-              UserName = "",
-              Email = "",
-              Photo = "",
-              Password = "",
-              FirstName = "",
-              LastName = "",
-              Projects = [],
-              PhoneNumber = "",
+              username = "",
+              email = "",
+              photo = "",
+              password = "",
+              firstname = "",
+              lastname = "",
+              projects = [],
+              phonenumber = "",
               state = "",
-              RefreshToken = ""}){
+              refreshtoken = ""}){
     
     this._id = _id;
-    this.username = UserName;
-    this.email = Email;
-    this.photo = Photo;
-    this.password = Password;
-    this.firstname = FirstName;
-    this.lastname = LastName;
-    this.projects = Projects;
-    this.phoneno = PhoneNumber;
+    this.username = username;
+    this.email = email;
+    this.photo = photo;
+    this.password = password;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.projects = projects;
+    this.phoneno = phonenumber;
     this.state = state;
-    this.refreshToken = RefreshToken;
+    this.refreshToken = refreshtoken;
   }
  
   getUserObject(){
   return({_id : this._id,
-          UserName : this.username,
-          Email : this.email,
-          Photo : this.photo,
-          Password : this.password,
-          FirstName : this.firstname,
-          LastName : this.lastname,
-          Projects : this.projects,
-          PhoneNumber : this.phoneno,
-          State : this.state,
-          RefreshToken : this.refreshToken});
+          userName : this.username,
+          email : this.email,
+          photo : this.photo,
+          password : this.password,
+          firstname : this.firstname,
+          lastname : this.lastname,
+          projects : this.projects,
+          phonenumber : this.phoneno,
+          state : this.state,
+          refreshtoken : this.refreshtoken});
   }
   
   setUserObject({_id = this._id,
@@ -52,7 +52,7 @@ class userObject{
                 projects = this.projects,
                 phoneno = this.phoneno,
                 state = this.state,
-                refreshToken = this.refreshToken}){
+                refreshtoken = this.refreshtoken}){
   
     this._id = _id;
     this.username = username;
@@ -64,7 +64,7 @@ class userObject{
     this.projects = projects;
     this.phoneno = phoneno;
     this.state = state;
-    this.refreshToken = refreshToken;
+    this.refreshtoken = refreshtoken;
   }
 }
 
