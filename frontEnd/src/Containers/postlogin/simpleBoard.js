fddce4dc52b0ed509ccd38b3dec41102a0328c5b
kinshuksri25 <42490429+kinshuksri25@users.noreply.github.com>
2020-03-24 11:09:17 +0530

    Delete simpleBoard.js

--- frontEnd/src/Containers/postlogin/simpleBoard.js
+++
@@ -1,8 +0,0 @@
-//Dependencies
-import React, { Component } from 'react';
-import { hot } from "react-hot-loader";
-
-export default function SimpleBoard () {
-
-        return (<div> "SimpleBoard" </div>);
-}
