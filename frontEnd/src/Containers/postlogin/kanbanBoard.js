5be4e09260207bdadd892fe7cf323c56a7418bf7
kinshuksri25 <42490429+kinshuksri25@users.noreply.github.com>
2020-03-24 12:48:23 +0530

    Update kanbanBoard.js

--- frontEnd/src/Containers/postlogin/kanbanBoard.js
+++ frontEnd/src/Containers/postlogin/kanbanBoard.js
@@ -7,10 +7,29 @@ class KanbanBoard extends Component {
 
         constructor(props){
                 super(props);
+                this.projectSelector = this.projectSelector.bind(this);
         }
+        
+        projectSelector(){
+        let projectID = window.location.pathname.substring(window.location.pathname.lastIndexOf('/')+1);
+        let projectObject = {};
+        this.props.user.projects.map(project => {
+            if(project._id == projectID)
+                 projectObject = project;      
+        });
+        JSON.stringify(projectObject) == JSON.stringify({}) && window.history.replaceState({}, "",urls.DASHBOARD);
+        return projectObject;
+    }      
         render(){
-                return (<div> {this.props.projectObject.title} </div>);
+                let projectObject = this.projectSelector();
+                return (<div>{projectObject.title}</div>);
         }
 }
 
-export default connect(null,null)(KanbanBoard);
+const mapStateToProps = (state) => {
+    return {
+        user: state.userStateReducer
+    }
+};
+
+export default connect(mapStateToProps,null)(KanbanBoard); 
